4a1b947ea13a6ac8a92d292119fe86defed1d1a7
Martin Prokopič <prokoma7@fit.cvut.cz>
2023-03-17 09:53:00 +0100

    Added SingleFunExit transform to get rid of multiple epilogues

--- src/main/scala/tinycc/common/Optimizer.scala
+++ src/main/scala/tinycc/common/Optimizer.scala
@@ -1,12 +1,13 @@
 package tinycc.common
 
 import tinycc.common.ir.{IrPrinter, IrProgram}
-import tinycc.common.transform.{AllocOrdering, BasicBlockScheduling, MemToReg}
+import tinycc.common.transform.{AllocOrdering, BasicBlockScheduling, MemToReg, SingleFunExit}
 
 class Optimizer extends ProgramTransform[IrProgram] {
   val allocOrdering = new AllocOrdering
   val basicBlockScheduling = new BasicBlockScheduling
   val memToReg = new MemToReg(removeLocals = true)
+  val singleFunExit = new SingleFunExit
 
   val printer = new IrPrinter()
 
@@ -16,6 +17,7 @@ class Optimizer extends ProgramTransform[IrProgram] {
     basicBlockScheduling.transformProgram(program)
     allocOrdering.transformProgram(program)
     memToReg.transformProgram(program)
+    singleFunExit.transformProgram(program)
 
     log("after\n" + printer.printToString(program))
   }
---
+++ src/main/scala/tinycc/common/transform/SingleFunExit.scala
@@ -0,0 +1,33 @@
+package tinycc.common.transform
+
+import tinycc.common.ProgramTransform
+import tinycc.common.ir.IrManipulation.replaceInsnWith
+import tinycc.common.ir.IrTy.VoidTy
+import tinycc.common.ir._
+
+/** If there are multiple blocks terminated by RetInsn or RetVoidInsn, create a new function exit block. */
+class SingleFunExit extends ProgramTransform[IrProgram] {
+  def transformFun(fun: IrFun): Unit = {
+    if (fun.returnTy == VoidTy) {
+      val retVoidInsns = fun.insns.collect({ case insn: RetVoidInsn => insn })
+      if (retVoidInsns.size > 1) {
+        val exitBlock = fun.append(new BasicBlock("exit", fun))
+        log(s"added new $exitBlock to $fun")
+        exitBlock.append(new RetVoidInsn(exitBlock))
+        retVoidInsns.foreach(insn => replaceInsnWith(insn, new BrInsn(exitBlock, insn.basicBlock)))
+      }
+    } else {
+      val retInsns = fun.insns.collect({ case insn: RetInsn => insn })
+      if (retInsns.size > 1) {
+        val exitBlock = fun.append(new BasicBlock("exit", fun))
+        log(s"added new $exitBlock to $fun")
+        val phi = exitBlock.append(new PhiInsn(retInsns.map(insn => (Some(insn.arg), Some(insn.basicBlock))).toIndexedSeq, exitBlock))
+        exitBlock.append(new RetInsn(phi, exitBlock))
+        retInsns.foreach(insn => replaceInsnWith(insn, new BrInsn(exitBlock, insn.basicBlock)))
+      }
+    }
+  }
+
+  override def transformProgram(program: IrProgram): Unit =
+    program.funs.foreach(transformFun)
+}
