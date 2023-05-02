0350c222284fcfe679f24192dee13a0dff0ec2fa
Martin Prokopič <prokoma7@fit.cvut.cz>
2023-05-02 17:40:09 +0200

    Make scripts more robust

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euo pipefail
-trap 'echo "Script error: $(basename "$BASH_SOURCE"):$LINENO $BASH_COMMAND" >&2' ERR
+trap 'echo "script error: $(basename "$BASH_SOURCE"):$LINENO $BASH_COMMAND" >&2' ERR
+script_dir="$(dirname -- "$(readlink -f -- "$0")")"
 
-root_dir="$(dirname "$0")/.."
+root_dir="$script_dir/.."
 
-which asdf &>/dev/null || . "$HOME/.asdf/asdf.sh"
+which asdf &>/dev/null || . "$HOME/.asdf/asdf.sh" || echo "asdf is not installed, using local sbt binary" >&2
 
 cd "$root_dir"
-sbt assembly
+sbt assembly || exit $?
